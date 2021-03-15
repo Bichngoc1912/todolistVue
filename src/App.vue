e4a7f5595109484fc2c08dff78fdbee58df7b632
Bich Ngoc <ngocntbCTK40@gmail.com>
2021-03-15 23:00:39 +0700

    update: code

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <headerMenu />
+    <todolist />
+    <footerTodo />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import todolist from './components/TodoList.vue'
+import headerMenu from './components/header.vue'
+import footerTodo from './components/footer.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    headerMenu,
+    todolist,
+    footerTodo
   }
 }
 </script>
@@ -21,8 +25,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
+import axios from 'axios'
 import Vue from 'vue'
 import App from './App.vue'
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  axios,
   render: h => h(App),
 }).$mount('#app')
+
+
